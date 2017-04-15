86f4e1beacd49e803358d22927c00b7a4a1ffc2b
Ivan Habunek <ivan@habunek.com>
2017-04-15 14:55:27 +0200

    Drop support for Python 2.6

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
