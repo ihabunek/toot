ee833d4569ffefeabd681980eb801d0117ce012c
Ivan Habunek <ivan@habunek.com>
2019-08-27 15:08:40 +0200

    Add reply, reblog and favourite counters

--- toot/tui/timeline.py
+++ toot/tui/timeline.py
@@ -171,6 +171,13 @@ class StatusDetails(urwid.Pile):
             yield ("pack", urwid.Divider())
             yield ("pack", self.build_linebox(self.card_generator(card)))
 
+        yield ("pack", urwid.AttrWrap(urwid.Divider("-"), "gray"))
+        yield ("pack", urwid.Text([
+            ("gray", "⤶ {} ".format(status.data["replies_count"])),
+            ("yellow" if status.reblogged else "gray", "♺ {} ".format(status.data["reblogs_count"])),
+            ("yellow" if status.favourited else "gray", "★ {}".format(status.data["favourites_count"])),
+        ]))
+
         # Push things to bottom
         yield ("weight", 1, urwid.SolidFill(" "))
         yield ("pack", urwid.Text([
