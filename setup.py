1b2a9c41988175163f7117811155be0358fb04c6
Ivan Habunek <ivan@habunek.com>
2020-01-21 09:37:21 +0100

    Bump version, changelog

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ and blocking accounts and other actions.
 
 setup(
     name='toot',
-    version='0.24.0',
+    version='0.25.0',
     description='Mastodon CLI client',
     long_description=long_description.strip(),
     author='Ivan Habunek',
