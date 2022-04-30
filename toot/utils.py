9b050cf7421bd21ac5603d0538cbe8744de13375
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2022-04-30 21:11:02 +0200

    Add sensitive to metacomments and proper roundtrip
    The command-line arguments are reset so that if the corresponding
    metacomment is removed the default value for the parameter is used
    again. This ensures that the output from the editor is consistent.

--- toot/utils.py
+++ toot/utils.py
@@ -12,6 +12,8 @@ from bs4 import BeautifulSoup
 
 from toot.exceptions import ConsoleError
 
+from argparse import FileType
+
 VISIBILITY_CHOICES = ['public', 'unlisted', 'private', 'direct']
 
 def language(value):
@@ -116,7 +118,7 @@ def multiline_input():
 EDITOR_INPUT_INSTRUCTIONS = """
 # Please enter your toot. Lines starting with '#' will be ignored, and an empty
 # message aborts the post.
-# Metadata comments can be set for media, description, lang, spoiler, reply-to, visibility
+# Metadata comments can be set for media, description, lang, spoiler, reply-to, visibility, sensitive
 """
 
 def parse_editor_meta(line, args):
@@ -131,19 +133,20 @@ def parse_editor_meta(line, args):
         return True
     # TODO override user with 'from'
     if key == 'media':
-        args.media = args.media or []
-        args.media.append(val)
+        args.media.append(FileType('rb')(val))
     elif key == 'description':
-        args.description = args.description or []
         args.description.append(val)
     elif key == 'lang' or key == 'language':
         args.language = language(val)
-    elif key == 'spoiler':
+    elif key == 'spoiler' or key == 'spoiler-text' or key == 'spoiler_text':
         args.spoiler_text = val
     elif key == 'reply-to' or key == 'reply_to' or key == 'replyto':
         args.reply_to = val
     elif key == 'visibility':
         args.visibility = visibility(val)
+    elif key == 'sensitive':
+        # 0, f, false, n will count s not-sensitive, any other value will be taken as a yes'
+        args.sensitive = val.lower() not in [ '0', 'f', 'false', 'n', 'no' ]
     else:
         print_out("Ignoring unsupported comment metadata <red>{}</red> with value <yellow>{}</yellow>.".format(key, val))
     return True
@@ -153,8 +156,42 @@ def parse_editor_input(args):
     """Lets user input text using an editor."""
 
     editor = args.editor
-    # TODO initialize metacomments from the args field
-    text = (args.text or "") + EDITOR_INPUT_INSTRUCTIONS
+    # initialize metacomments from the args field, and reset them in args so that
+    # they get reset if removed from the metacomments
+    meta = ""
+    if args.reply_to:
+        meta += "# reply_to: " + args.reply_to + "\n"
+        args.reply_to = None
+    if args.visibility:
+        meta += "# visibility: " + args.visibility + "\n"
+        args.visibility = 'public' # TODO can we take the default from the command definition?
+    if args.language:
+        meta += "# lang: " + args.language + "\n"
+        args.language = None
+    if args.spoiler_text:
+        meta += "# spoiler: " + args.spoiler_text + "\n"
+        args.spoiler_text = None
+    if args.sensitive:
+        meta += "# sensitive: " + args.sensitive + "\n"
+        args.sensitive = False
+
+    media = args.media or []
+    descriptions = args.description or []
+    if len(media) > 0:
+        for idx, file in enumerate(media):
+            meta += "# media: " + file.name + "\n"
+            # encourage the user to add descriptions, always present the meta field
+            desc = descriptions[idx].strip() if idx < len(descriptions) else ''
+            meta += "# description: " + desc + "\n"
+    if len(descriptions) > len(media):
+        for idx, desc in enumerate(descriptions):
+            if idx < len(media):
+                continue
+            meta += "# description: "+ descriptions[idx].strip() + "\n"
+
+    args.media = []
+    args.description = []
+    text = meta + (args.text or "") + EDITOR_INPUT_INSTRUCTIONS
 
     # loop to avoid losing text if something goes wrong (e.g. wrong visibility setting)
     with tempfile.NamedTemporaryFile() as f:
@@ -172,6 +209,11 @@ def parse_editor_input(args):
                 lines = text.strip().splitlines()
                 lines = (l for l in lines if not parse_editor_meta(l, args))
                 args.text = "\n".join(lines)
+
+                # sanity check: this wll be checked by post() too, but we want to catch this early so that
+                # the user can still fix things in the editor
+                if args.media and len(args.media) > 4:
+                    raise ConsoleError("Cannot attach more than 4 files.")
             except Exception as e:
                 text += "\n#" + str(e)
                 continue
