8bd764659f553c05b833aa248d5c93c97e2f8a24
Dan Schwarz <schwarz.dan@gmail.com>
2023-02-26 22:03:35 -0500

    Make better use of screen real estate for wide/short images

--- toot/tui/timeline.py
+++ toot/tui/timeline.py
@@ -349,26 +349,38 @@ class StatusDetails(urwid.Pile):
             if status else ())
         return super().__init__(widget_list)
 
-    def image_widget(self, path, rows=None) -> urwid.Widget:
+    def image_widget(self, path, rows=None, aspect=None) -> urwid.Widget:
         """Returns a widget capable of displaying the image
 
         path is required; URL to image
-        rows, if specfied, sets a fixed number of rows.
-        """
+        rows, if specfied, sets a fixed number of rows. Or:
+        aspect, if specified, calculates rows based on pane width
+        and the aspect ratio provided"""
 
         if not rows:
+            if not aspect:
+                aspect = 3 / 2  # reasonable default
+
             screen_rows = screen.get_cols_rows()[1]
             if self.timeline.can_render_pixels:
                 # for pixel-rendered images,
                 # image rows should be 33% of the available screen
                 # but in no case fewer than 10
-                rows = max(10, math.floor(screen_rows * 0.33))
-
+                rows = max(10, math.floor(screen_rows * .33))
             else:
-                # for cell-rendered images, try to render using
-                # all available rows (assume 6 are used by TUI)
-                # but in no case fewer than 10
-                rows = max(10, screen_rows - 6)
+                # for cell-rendered images,
+                # use the max available columns
+                # and calculate rows based on the image
+                # aspect ratio
+                cols = math.floor(0.55 * screen.get_cols_rows()[0])
+                rows = math.ceil((cols / 2) / aspect)
+
+                # if the calculated rows are more than will
+                # fit on one screen, reduce to one screen of rows
+                rows = min(screen_rows - 6, rows)
+
+                # but in no case fewer than 10 rows
+                rows = max(rows, 10)
 
         img = None
         if hasattr(self.timeline, "images"):
@@ -435,7 +447,11 @@ class StatusDetails(urwid.Pile):
                     if m["url"]:
                         if m["url"].lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp')):
                             yield urwid.Text("")
-                            yield self.image_widget(m["url"])
+                            try:
+                                aspect = float(m["meta"]["original"]["aspect"])
+                            except Exception:
+                                aspect = None
+                            yield self.image_widget(m["url"], aspect=aspect)
                             yield urwid.Divider()
                         yield ("pack", urwid.Text(("link", m["url"])))
 
@@ -503,7 +519,11 @@ class StatusDetails(urwid.Pile):
         if card["image"]:
             if card["image"].lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp')):
                 yield urwid.Text("")
-                yield self.image_widget(card["image"])
+                try:
+                    aspect = int(card["width"]) / int(card["height"])
+                except Exception:
+                    aspect = None
+                yield self.image_widget(card["image"], aspect=aspect)
 
     def poll_generator(self, poll):
         for idx, option in enumerate(poll["options"]):
