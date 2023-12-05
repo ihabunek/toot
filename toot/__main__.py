d91f3477a8f24f5f9595c9953ada552ff348b1ca
Ivan Habunek <ivan@habunek.com>
2023-12-05 10:41:54 +0100

    Simplify main
    No need to handle this stuff here

--- toot/__main__.py
+++ toot/__main__.py
@@ -1,15 +1,3 @@
-import sys
 from toot.cli import cli
-from toot.exceptions import ConsoleError
-from toot.output import print_err
-from toot.settings import load_settings
 
-try:
-    defaults = load_settings().get("commands", {})
-    cli(default_map=defaults)
-except ConsoleError as ex:
-    print_err(str(ex))
-    sys.exit(1)
-except KeyboardInterrupt:
-    print_err("Aborted")
-    sys.exit(1)
+cli()
