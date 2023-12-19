0e41abf21348f690dfb8d398114d5f032180562f
Daniel Schwarz <schwarz.dan@gmail.com>
2023-12-19 17:10:54 -0500

    Revert change to terminal queries as it disables kitty pixel images

--- toot/tui/images.py
+++ toot/tui/images.py
@@ -1,8 +1,6 @@
 # If term_image is loaded use their screen implementation which handles images
 try:
     from term_image.widget import UrwidImageScreen
-    from term_image import disable_queries
-    disable_queries()  # prevent phantom keystrokes from terminal queries
     TuiScreen = UrwidImageScreen
 except ImportError:
     from urwid.raw_display import Screen
