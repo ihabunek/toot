84e75347e0c81538450731d171d3a9418cbfe32f
Ivan Habunek <ivan@habunek.com>
2023-12-31 21:29:06 +0100

    Make palettes work again

--- toot/cli/__init__.py
+++ toot/cli/__init__.py
@@ -55,11 +55,8 @@ def get_default_map():
     # TODO: remove in version 1.0
     tui_old = settings.get("tui", {})
 
-    # Remove palette to avoid triggering warning for still valid [tui.palette] section
-    if "palette" in tui_old:
-        del tui_old["palette"]
-
     if tui_old:
+        # TODO: don't show the warning for [toot.palette]
         print_warning("Settings section [tui] has been deprecated in favour of [commands.tui].")
         tui_new = commands.get("tui", {})
         commands["tui"] = {**tui_old, **tui_new}
