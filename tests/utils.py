968a516f76b8821bc54fcd13f4990a404bccbf00
Ivan Habunek <ivan@habunek.com>
2024-04-06 15:06:59 +0200

    Remove unused helpers

--- tests/utils.py
+++ tests/utils.py
@@ -9,24 +9,6 @@ from typing import Callable, TypeVar
 T = TypeVar("T")
 
 
-class MockResponse:
-    def __init__(self, response_data={}, ok=True, is_redirect=False):
-        self.response_data = response_data
-        self.content = response_data
-        self.ok = ok
-        self.is_redirect = is_redirect
-
-    def raise_for_status(self):
-        pass
-
-    def json(self):
-        return self.response_data
-
-
-def retval(val):
-    return lambda *args, **kwargs: val
-
-
 def run_with_retries(fn: Callable[..., T]) -> T:
     """
     Run the the given function repeatedly until it finishes without raising an
