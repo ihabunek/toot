40acf7dd248ad190167b26cd31d1004d53543ea8
Ivan Habunek <ivan@habunek.com>
2026-02-07 10:37:57 +0100

    Remove version test
    It basically uses the same code to test the version as is used to
    generate it so it does not help.

--- tests/test_version.py
+++
@@ -1,8 +0,0 @@
-import toot
-from pkg_resources import get_distribution
-
-
-def test_version():
-    """Version specified in __version__ should be the same as the one
-    specified in setup.py."""
-    assert toot.__version__ == get_distribution('toot').version
